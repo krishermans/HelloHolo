3522eddad4d19d6ad6a20a29f2acf434204e8720
Kris Hermans <Kris.Hermans@live.be>
2017-04-14 15:51:02 +0200

    added Tap gesture

---
+++ Assets/MyAssets/Scripts/GestureALot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class GestureALot : MonoBehaviour, IInputClickHandler, IFocusable
+{
+    public Material greyMaterial;
+    public Material redMaterial;
+
+    public void OnFocusEnter()
+    {
+        this.GetComponent<Renderer>().material = redMaterial;
+    }
+
+    public void OnFocusExit()
+    {
+        this.GetComponent<Renderer>().material = greyMaterial;
+    }
+
+    public void OnInputClicked(InputEventData eventData)
+    {
+        Debug.Log("OnInputClicked");
+        this.GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
